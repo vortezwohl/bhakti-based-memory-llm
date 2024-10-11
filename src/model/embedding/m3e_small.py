cc63154b2e08c833b4ad0a05f92ed2074629fdb9
zihaowu <zihao.wu@igg.com>
2024-10-11 18:08:55 +0800

    优化prompt

--- src/model/embedding/m3e_small.py
+++ src/model/embedding/m3e_small.py
@@ -1,4 +1,4 @@
 # 512 dimensions
 from sentence_transformers import SentenceTransformer
 
-m3e_small = SentenceTransformer('moka-ai/m3e-small')
+m3e_small = SentenceTransformer('moka-ai/m3e-small', cache_folder='./cache')
--- src/prompt/default_prompt.py
+++ src/prompt/default_prompt.py
@@ -2,7 +2,7 @@ from langchain_core.prompts import ChatPromptTemplate
 
 default_prompt = ChatPromptTemplate.from_template("""
 <System Prompt>
-    <Role>智能问答机器人</Role>
+    <Role>具备情感感知的智能助手</Role>
     <Characteristic>严谨、诚实</Characteristic>
     <Name>Calchas</Name>
     <Task>严格依据<System Prompt/>中的设定，结合<Memory/>中的上下文内容，对<Human Query/>进行回复</Task>
@@ -10,6 +10,7 @@ default_prompt = ChatPromptTemplate.from_template("""
         <1>给出尽可能简短的回答</1>
         <2>给出严谨的回答，当你无法确定问题的答案，请承认你的无知，不要给出毫无根据的答案</2>
         <3>你的回复文本末尾不应有换行符</3>
+        <4>使用和Human Query相同的语言进行回复</4>
     </Restraint>
 </System Prompt>
 <Memory>
