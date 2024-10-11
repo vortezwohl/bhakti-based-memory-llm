48194c9934f48cc0aae63f450b9d1c8ef7fe564a
zihaowu <zihao.wu@igg.com>
2024-10-11 12:56:03 +0800

    init

---
+++ bhakti_server/m3e_small/m3e_small_bhakti.py
@@ -0,0 +1,18 @@
+from bhakti import BhaktiServer
+from bhakti.database import DBEngine
+
+if __name__ == '__main__':
+    bhakti_server = BhaktiServer(
+        dimension=512,  # required, only vectors with 1024 dimensions are acceptable
+        db_path='../../database/m3e_small_db',  # required, path where stores data, portable
+        db_engine=DBEngine.DIPAMKARA,  # optional, default to dipamkara
+        cached=False,  # optional, default to false
+        host='0.0.0.0',  # optional, default to 0.0.0.0
+        port=23860,  # optional, default to 23860
+        eof=b'<eof>',  # optional, default to b'<eof>'
+        timeout=4.0,  # optional, default to 4.0 seconds
+        buffer_size=256,  # optional, default to 256 bytes
+        verbose=True  # optional, default to false
+    )
+    # run server
+    bhakti_server.run()
